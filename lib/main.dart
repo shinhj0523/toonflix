14b0ebfb44d32a9e3ca89a11982ee2dfd86a3861
신희준 <sinhuijun@sinhuijun-ui-MacBookPro-2.local>
2023-06-29 22:44:30 +0900

    reusable cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/widgets/button.dart';
+import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
   runApp(const App());
@@ -41,7 +42,7 @@ class App extends StatelessWidget {
               ],
             ),
             const SizedBox(
-              height: 120,
+              height: 80,
             ),
             Text(
               'Total Balance',
@@ -77,7 +78,7 @@ class App extends StatelessWidget {
               ],
             ),
             const SizedBox(
-              height: 100,
+              height: 50,
             ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.end,
@@ -103,67 +104,31 @@ class App extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            Container(
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                color: const Color(0xFF1F2123),
-                borderRadius: BorderRadius.circular(25),
+            const CurrencyCard(
+              name: "Euro",
+              code: "EUR",
+              amount: '6 428',
+              icon: Icons.euro,
+              isInverted: false,
+            ),
+            Transform.translate(
+              offset: const Offset(0, -20),
+              child: const CurrencyCard(
+                name: "Bitcoin",
+                code: "BTC",
+                amount: '9 785',
+                icon: Icons.currency_bitcoin,
+                isInverted: true,
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 10,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Euro',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 32,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              '6 428',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 5,
-                            ),
-                            Text(
-                              'EUR',
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.white),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                    Transform.scale(
-                      scale: 2.2,
-                      child: Transform.translate(
-                        offset: const Offset(-10, 7),
-                        child: const Icon(
-                          Icons.euro_rounded,
-                          color: Colors.white,
-                          size: 70,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+            ),
+            Transform.translate(
+              offset: const Offset(0, -40),
+              child: const CurrencyCard(
+                name: "Dollar",
+                code: "USD",
+                amount: '428',
+                icon: Icons.attach_money,
+                isInverted: false,
               ),
             ),
           ]),
